3a3f6511b470581a991241a21242b0e7a7399648
Frankie Bukenya <frankie.bukenya@gmail.com>
2019-06-08 13:06:55 +0300

    Add final code

--- src/js/index.js
+++ src/js/index.js
@@ -15,12 +15,14 @@ const bands = [
   'An Old Dog',
 ];
 
-const sortedBands = bands.sort(function(a,b) {
-    if(a > b) {
-        return 1;
-    } else {
-        return -1;
-    }
-});
+function strip(bandName) {
+    return bandName.replace(/^(a | the | an)/i , '').trim();
+}
+
+const sortedBands = bands.sort((a,b) => strip(a) > strip(b) ? 1 : -1);
+document.querySelector('#bands').innerHTML=
+sortedBands
+  .map(band => `<li>${band}</li>`)
+  .join(''); 
 
 console.log(sortedBands);
